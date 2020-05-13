d53ba578605149eab21a017a3b2aa1dcf36e9dba
github username <github email address>
2020-05-13 22:06:27 +0100

    contact done

---
+++ index.js
@@ -0,0 +1,23 @@
+const proj1 = document.getElementById('project1')
+const proj1div = document.getElementById('proj1Div')
+
+const proj2 = document.getElementById('project2')
+const proj2div = document.getElementById('proj2Div')
+
+
+proj1.addEventListener('click', ()=> {
+    if ( proj1div.className === 'projectInfo' ){
+        proj1div.className = 'projectInfoShow';
+    } else {
+        proj1div.className = 'projectInfo'
+    }
+ 
+})
+
+proj2.addEventListener('mouseover',() => {
+    if ( proj1div.className === 'projectInfo' ){
+        proj1div.className = 'projectInfoShow';
+    } else {
+        proj1div.className = 'projectInfo'
+    }
+})

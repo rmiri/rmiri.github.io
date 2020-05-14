4847229efe492b7e48babb84436491b8e5f20c6e
github username <github email address>
2020-05-14 13:03:25 +0100

    css mobile finished

--- index.js
+++ index.js
@@ -4,6 +4,27 @@
 // const proj2 = document.getElementById('project2')
 // const proj2div = document.getElementById('proj2Div')
 
+const photoDiv = document.getElementsByClassName('photoDiv')
+const projectInfo = document.getElementsByClassName('projectInfo')
+
+
+const myFunction = (j) => {
+    // console.log('hellu')
+    console.log(projectInfo[j])
+    // projectInfo[i].className = 'projectInfoVisible';
+}
+
+for (let i = 0; i < photoDiv.length; i++) {
+    // console.log(projectInfo[i])
+    photoDiv[i].addEventListener('click', function(){
+        if (projectInfo[i].style.display === 'none'){
+            projectInfo[i].style.display = 'inline-block';
+        } else {
+            projectInfo[i].style.display = 'none'
+        }
+    });
+}
+
 
 // proj1.addEventListener('click', ()=> {
 //     if ( proj1div.className === 'projectInfo' ){
@@ -21,3 +42,4 @@
 //         proj1div.className = 'projectInfo'
 //     }
 // })
+
